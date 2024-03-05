d322f35f942d75756e4ad87bae10ca03807d0217
e11sy <Egoramurin@gmail.com>
2024-03-05 18:35:36 +0300

    set up project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <Header />
+    <Layout>
+        <router-view />
+    </Layout>
+</template>
+
+<style lang="postcss">
+html,
+body {
+    background-color: #805626;
+}
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        '@/': '/src/',
+      },
+    },
+  };
+});
